205a896065593a7998ef028107efae7cf429a8de
joaopeltzchaves-droid <joao.peltz.chaves@escola.pr.gov.br>
2026-06-11 09:04:04 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+  const botao =  document.querySelector("button")
+    botao.addEventListener('click', curtir)
+    
+    function curtir(){
+       let curtidas = document.querySelector("span")
+       curtidas.textContent++;
+       }
+
+
+       //BOTÃO DO TAMANHO
+       const tamanho = document.getAnimations("tamanho")
+
+
+       //Função 
+       function aumentarFonte(){
+         fonte++;
+         document.body.style.fontsize =
+       }
