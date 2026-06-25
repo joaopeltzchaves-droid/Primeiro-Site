71c3fcf9fab7a3a999c7c1666d3bea6b75a1e9ac
joaopeltzchaves-droid <joao.peltz.chaves@escola.pr.gov.br>
2026-06-25 08:56:48 -0300

    centralizado e UI e UX

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-  const botao =  document.querySelector("button")
-    botao.addEventListener('click', curtir)
-    
-    function curtir(){
-       let curtidas = document.querySelector("span")
-       curtidas.textContent++;
-       }
+const botao = document.querySelector("button")
+        botao.addEventListener('click', curtir)
+
+        function curtir(){
+           let curtidas = document.querySelector("span")
+           curtidas.textContent++;
+        }
 
 
-       //BOTÃO DO TAMANHO
-       const tamanho = document.getAnimations("tamanho")
+        // BOTÃO DO TAMANHO
+        const tamanho = document.getElementById("tamanho")
+        tamanho.addEventListener("click", aumentarFonte)
+        let fonte = 16;
+        // FUNÇÃO 
+        function aumentarFonte(){
+            fonte++;
+            document.body.style.fontSize = fonte + "px";
+        }
+        // Botão Contraste
+        const contraste = document.getElementById("contraste")
+        contraste.addEventListener("click", mudarContraste)
 
-
-       //Função 
-       function aumentarFonte(){
-         fonte++;
-         document.body.style.fontsize =
-       }
+        // FUNÇÃO DO CONSTRATE
+        function mudarContraste(){
+            document.body.style.color = 'yellow'
+        }
